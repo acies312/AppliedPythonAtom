05b11b8d961fa69e2b434943d19fd64f13f72c87
Zhalil <alymkulov.j@yandex.ru>
2019-03-07 07:29:30 +0300

    07.03.19

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -10,4 +10,22 @@ def is_bracket_correct(input_string):
     :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
     :return: True or False
     '''
+
+    k = 0
+    a = input_string
+    if len(a) != 0:
+        if a[0] == ")" or a[0] == "]" or a[0] == "}":
+            return False
+        else:
+            while k < len(input_string):
+                if a[k] == "(" or a[k] == "[" or a[k] == "{":
+                    s = a[k] + a[k + 1]
+                    if s == "()" or s == "[]" or s == "{}":
+                        k += 2
+                    else:
+                        return False
+
+    else:
+        return True
+
     raise NotImplementedError
